8e4b476a8ee40d9a5b3d580a701efb25b071babd
DragonJT <jonathanthoms006@gmail.com>
2024-03-26 16:50:11 +0000

    draw animating gradient

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,58 @@
 #include "windows.h"
+#include <stdint.h>
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
 
-bool destroyed = false;
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+typedef int64_t int64;
+
+const int bytesPerPixel = 4;
+static bool running;
+static BITMAPINFO bitmapInfo;
+static void* bitmapMemory;
+static int bitmapWidth;
+static int bitmapHeight;
+
+void RenderWeirdGradient(int xOffset, int yOffset){
+    int pitch = bitmapWidth*bytesPerPixel;
+    uint8* row = (uint8*)bitmapMemory;
+    for(int y=0;y<bitmapHeight;y++){
+        uint32* pixel = (uint32*)row;
+        for(int x=0;x<bitmapWidth;x++){
+            uint8 blue = x+xOffset;
+            uint8 green = y+yOffset;
+            *pixel++ = ((green << 8) | blue);
+        }
+        row+=pitch;
+    }
+}
+
+void ResizeDIBSection(int width, int height){
+    if(bitmapMemory){
+        VirtualFree(bitmapMemory, 0, MEM_RELEASE);   
+    }
+    bitmapInfo.bmiHeader.biSize = sizeof(bitmapInfo.bmiHeader);
+    bitmapInfo.bmiHeader.biWidth = width;
+    bitmapInfo.bmiHeader.biHeight = -height;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 32;
+    bitmapInfo.bmiHeader.biCompression = BI_RGB;
+    bitmapWidth = width;
+    bitmapHeight = height;
+    int bitmapMemorySize = width*height*bytesPerPixel;
+    bitmapMemory = VirtualAlloc(0, bitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
+}
+
+void _UpdateWindow(HDC deviceContext, RECT* clientRect, int x, int y, int width, int height){
+    //StretchDIBits(deviceContext, x, y, width, height, x, y, width, height, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+    int windowWidth = clientRect->right - clientRect->left;
+    int windowHeight = clientRect->bottom - clientRect->top;
+    StretchDIBits(deviceContext, 0, 0, bitmapWidth, bitmapHeight, 0, 0, windowWidth, windowHeight, bitmapMemory, &bitmapInfo, DIB_RGB_COLORS, SRCCOPY);
+}
 
 LRESULT MainWindowCallback(
   HWND window,
@@ -12,13 +64,18 @@ LRESULT MainWindowCallback(
     LRESULT result = 0;
     switch(message){
         case WM_SIZE:{
-
+            RECT clientRect;
+            GetClientRect(window, &clientRect);
+            int width = clientRect.right - clientRect.left;
+            int height = clientRect.bottom - clientRect.top;
+            ResizeDIBSection(width, height);
         } break;
         case WM_DESTROY:{
-            destroyed = true;
+            running = false;
             result = DefWindowProc(window, message, wparam, lparam);
         } break;
         case WM_CLOSE:{
+            running = false;
             result = DefWindowProc(window, message, wparam, lparam);
         }break;
         case WM_ACTIVATEAPP:{
@@ -29,9 +86,11 @@ LRESULT MainWindowCallback(
             HDC deviceContext = BeginPaint(window, &paint);
             int x = paint.rcPaint.left;
             int y = paint.rcPaint.top;
-            int height = paint.rcPaint.bottom - paint.rcPaint.top;
             int width = paint.rcPaint.right - paint.rcPaint.left;
-            PatBlt(deviceContext, x, y, width, height, WHITENESS);
+            int height = paint.rcPaint.bottom - paint.rcPaint.top;
+            RECT clientRect;
+            GetClientRect(window, &clientRect);
+            _UpdateWindow(deviceContext,&clientRect,x,y,width,height);
             EndPaint(window, &paint);
         }break;
         default:{
@@ -55,7 +114,7 @@ int WinMain(
 
     RegisterClass(&windowClass);
 
-    HWND windowHandle = CreateWindowEx(
+    HWND window = CreateWindowEx(
         0,
         windowClass.lpszClassName, 
         "Window",
@@ -69,13 +128,28 @@ int WinMain(
         instance,
         0);
 
-    MSG message;
-    while(GetMessage(&message, 0, 0, 0) > 0){
-        if(destroyed){
-            return 0;
+    running = true;
+    int xOffset = 0;
+    int yOffset = 0;
+    while(running){
+        MSG message;
+        while(PeekMessage(&message, 0, 0, 0, PM_REMOVE)){
+            if(message.message == WM_QUIT){
+                running = false;
+            }
+            TranslateMessage(&message);
+            DispatchMessage(&message);
         }
-        TranslateMessage(&message);
-        DispatchMessage(&message);
+        HDC deviceContext = GetDC(window);
+        RenderWeirdGradient(xOffset, yOffset);
+        RECT clientRect;
+        GetClientRect(window, &clientRect);
+        int width = clientRect.right - clientRect.left;
+        int height = clientRect.bottom - clientRect.top;
+        _UpdateWindow(deviceContext,&clientRect,0,0,width,height);
+        ReleaseDC(window, deviceContext);
+
+        xOffset++;
     }
 
     return 0;
